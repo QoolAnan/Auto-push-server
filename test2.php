a74beca2dea120c5e67259ba4fd51f85680bcbd3
QoolAtive <QoolAtive@192.168.1.127>
2013-07-16 11:10:32 +0700

    edit test1,2,4

--- test2.php
+++ test2.php
@@ -1,5 +1,5 @@
 <?php
-echo "test2";
+echo "test2-1";
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
--- test4.php
+++ test4.php
@@ -1,5 +1,5 @@
 <?php
-echo "test4";
+echo "test4-1";
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
